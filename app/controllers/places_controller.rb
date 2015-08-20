9229bffbff16de3fa417e46047e037115b3f63c1
Daniel Rotenberg <Daniel.Rotenberg88@gmail.com>
2015-08-19 23:52:06 -0400

    Completed the features for the submit button. This allows users to add new reviews and links back to the homepage after database updated

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,7 +9,10 @@ class PlacesController < ApplicationController
 
 	def create
 		Place.create(place_params)
+		redirect_to root_path
 	end
+
+	private
 
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
